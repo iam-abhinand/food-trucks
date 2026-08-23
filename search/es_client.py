bd6d52dc05722d81b0945d28ae5683178c990e6a
Abhinand Satheesh <abhinandask@gmail.com>
2026-08-23 15:51:24 +0530

    feat: support authenticated elasticsearch connections for cloud deployment

--- search/es_client.py
+++ search/es_client.py
@@ -1,4 +1,15 @@
+"""
+Shared Elasticsearch client instance, configured from settings.
+"""
+
 from django.conf import settings
 from elasticsearch import Elasticsearch
 
-es_client = Elasticsearch(hosts=[settings.ELASTICSEARCH_HOST])
+if settings.ELASTICSEARCH_USERNAME and settings.ELASTICSEARCH_PASSWORD:
+    es_client = Elasticsearch(
+        hosts=[settings.ELASTICSEARCH_HOST],
+        basic_auth=(settings.ELASTICSEARCH_USERNAME, settings.ELASTICSEARCH_PASSWORD),
+    )
+else:
+    # Local dev: security disabled, no auth needed.
+    es_client = Elasticsearch(hosts=[settings.ELASTICSEARCH_HOST])
